f0419af3d8f56b94dc2a80abdea33b584bc655fb
Raining <raining@Rainings-MacBook-Pro.local>
2017-06-27 11:23:50 -0400

    ImmiCalc 1. Added a view for landing date selection 2. Added and tested date picker functions 3. No algorithm 4. Global variables and constants are now stored in "vars"
    TODO
    1. Hide date picker when tap outside
    2. Main tasks: new views for adding dates, and result display
    3. Main task: implement immigration algorithms

--- ImmiCalc/ViewController.swift
+++ ImmiCalc/ViewController.swift
@@ -7,19 +7,29 @@
 //
 
 import UIKit
-var pr_citi_flag:Int = 0
+
+// Global variables
+struct vars {
+    static let formatter = DateFormatter()
+    static var pr_citi_flag:Int = 0
+    static var land_date = Date()
+}
+
 class ViewController: UIViewController {
     @IBOutlet weak var pr_button: UIButton!
     @IBOutlet weak var citi_button: UIButton!
     
+    // Set pr_citi_flag
     @IBAction func Citi_button_pressed(_ sender: UIButton) {
-        pr_citi_flag = 1
+        vars.pr_citi_flag = 1
     }
     @IBAction func PR_button_pressed(_ sender: UIButton) {
-        pr_citi_flag = 0
+        vars.pr_citi_flag = 0
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Make rounded corners for buttons
         pr_button.layer.cornerRadius = 10
         pr_button.layer.borderWidth = 0
         citi_button.layer.cornerRadius = 10
--- ImmiCalc/ViewController2.swift
+++ ImmiCalc/ViewController2.swift
@@ -8,21 +8,47 @@
 
 import UIKit
 
-class ViewController2: UIViewController {
+class ViewController2: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet weak var testLabel: UILabel!
-    
+    @IBOutlet weak var done_button: UIButton!
+    @IBOutlet weak var land_label: UILabel!
+    @IBOutlet weak var land_text: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        if (pr_citi_flag == 0) {
-            testLabel.text = "Permanent Resident"
+        // Make rounded corners for done button
+        done_button.layer.cornerRadius = 10
+        done_button.layer.borderWidth = 0
+        
+        // Date formattor initialization and storing landing date
+        vars.formatter.dateStyle = DateFormatter.Style.medium
+        vars.formatter.timeStyle = DateFormatter.Style.none
+        land_text.text = vars.formatter.string(from: vars.land_date)
+        
+        // Date picker initialization and color changes
+        let datePicker:UIDatePicker = UIDatePicker()
+        datePicker.backgroundColor = .white
+        datePicker.setValue(UIColor.red, forKey: "textColor")
+        datePicker.datePickerMode = UIDatePickerMode.date
+        datePicker.addTarget(self, action: #selector(ViewController2.datePickerValueChanged), for: UIControlEvents.valueChanged)
+        land_text.inputView = datePicker
+        // Input text field with the datepicker
+        
+        // Testing pr_citi_flag
+        /*if (vars.pr_citi_flag == 0) {
+            land_label.text = "Permanent Resident"
         }
         else {
-            testLabel.text = "Citizen"
-        }
+            land_label.text = "Citizen"
+        }*/
         // Do any additional setup after loading the view.
     }
-
+    
+    func datePickerValueChanged(sender:UIDatePicker) {
+        // Format, store and display the selected date every time datepicker is changed
+        vars.land_date = sender.date
+        land_text.text = vars.formatter.string(from: vars.land_date)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
